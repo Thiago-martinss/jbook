18edd7609f299864c965bb331cb1cab129ae22e3
Thiago <martinsmthiago@gmail.com>
2024-05-27 20:38:35 -0300

    fixed typescript errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,59 @@
-import { useState, useEffect, useRef } from "react";
 import * as esbuild from 'esbuild-wasm';
-import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from "react-dom/client";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-    const ref = useRef<any>();
-    const [input, setInput] = useState("")
-    const [code, setCode] = useState("")
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker:true,
-            wasmURL: '/esbuild.wasm'
-        });
-        
-    };
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-    useEffect(() => {
-        startService();
-    }, []);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
-    const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin()]
-        })
-        setCode(result.code);
-    };
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <pre>{code}</pre>
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+root.render(<App />);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,31 +1,70 @@
-import * as esbuild from 'esbuild-wasm';
-     
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "filecache",
+});
+
+(async () => {
+  await fileCache.setItem("color", "red");
+})();
+
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
-      });
- 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        if (args.path === 'index.js') {
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            loader: 'jsx',
-            contents: `
-              import message from './message';
-              console.log(message);
-            `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
+            namespace: "a",
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: `
+              import React, { useState } from 'react';
+              console.log(React, useState);
+            `,
+          };
+        }
+
+        const chachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if (chachedResult) {
+          return chachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
