c54b10af4b22c36bf46fce32b623d30ea94774d2
Thiago <martinsmthiago@gmail.com>
2024-05-27 21:01:55 -0300

    refactor unpkg-path to fetch-plugin

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 const el = document.getElementById("root");
+
 
 const root = ReactDOM.createRoot(el!);
 
@@ -30,7 +32,10 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
